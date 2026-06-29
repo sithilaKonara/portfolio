ee516f4a64ac0e7eac502a4efe825d15de9b492d
Sithila Konara <sithila@yahoo.com>
2026-06-29 23:38:56 +0300

    inital file push

--- portfolio-scaffold/portfolio/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://yourdomain.com',
-  compressHTML: true,
-  build: {
-    assets: '_assets',
-  },
-});
